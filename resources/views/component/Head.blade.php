abc47bf2c9cba677cd3fc7fca982c854005f4dca
Figo Akbar <figoakbar15@gmail.com>
2021-03-18 17:44:44 +0700

    Create Head.blade.php

---
+++ resources/views/component/Head.blade.php
@@ -0,0 +1,16 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<!-- Styles -->
+<link href="/css/main.css" rel="stylesheet">
+
+<!-- Font -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Khula:wght@700;800&display=swap" rel="stylesheet">
+
+
+<title>ScoreNow</title>
