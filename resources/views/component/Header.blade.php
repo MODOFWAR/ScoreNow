9bb33778ceeaf4ce515e12d9326b9d64a9cde6a1
Figo Akbar <figoakbar15@gmail.com>
2021-03-18 17:45:03 +0700

    Create Header.blade.php

---
+++ resources/views/component/Header.blade.php
@@ -0,0 +1,55 @@
+<div class="container1">
+        <nav class="navbar navbar-expand-lg navbar-light navbartop py-3">
+            <div class="container-fluid">
+              <a class="navbar-brand scorenow" href="#">ScoreNow</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse ms-auto" id="navbarSupportedContent">
+                </form>
+                <div class="collapse navbar-collapse" id="navbarNav1">
+                    <ul class="navbar-nav ms-auto">
+                      <li class="nav-item navbar2">
+                      <form class="d-flex">
+                      <input class="form-control me-5 search" type="search" placeholder="Search..." aria-label="Search">  
+                      <li class="nav-item navbar2">
+                      <button class="btn btn login" type="submit">Login</button>
+                      </li>
+                      <li class="nav-item navbar2">
+                      <button class="btn btn tombol" type="submit">Register</button>
+                    </ul>
+                  </div>
+        
+              </div>
+            </div>
+        </nav>
+          
+      <div class="container2">
+        <nav class="navbar navbar-expand-lg navbar-light" style= "background-color: #FFFFFF;">
+        <div class="container-fluid">
+          
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav1">
+            <ul class="navbar-nav mx-auto ">
+              <li class="nav-item navbar2">
+                <a class="nav-link active homea" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item navbar2">
+                <a class="nav-link" href="#">News</a>
+              </li>
+              <li class="nav-item navbar2">
+                <a class="nav-link" href="#">Top Scorer</a>
+              </li>
+              <li class="nav-item navbar2">
+                <a class="nav-link" href="#">Top assist</a>
+              </li>
+              <li class="nav-item navbar2">
+                <a class="nav-link" href="#">Highlight Video</a>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>  
+    </nav>
