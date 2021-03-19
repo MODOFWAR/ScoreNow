8aa4dc885ddb8ce53693b16f6bd703cedec59304
Figo Akbar <figoakbar15@gmail.com>
2021-03-19 12:01:27 +0700

    Create Footer.blade.php

---
+++ resources/views/component/Footer.blade.php
@@ -0,0 +1,34 @@
+<div class='footer mt-5'>
+<div class="container">
+    <div class="row">
+        <div class="col-6 pt-5">
+                <h4 class = "footer_title">About Us</h4>
+                    <h5>Company</h5>
+                    <h5>Careers</h5>
+                    <h5>Contact</h5>
+            <div class="row pt-4 pb-4">
+                <h4 class = "footer_title">Legal</h4>
+                <div class="row pt-2">
+                    <h5>Privacy Policy</h5>
+                    <h5>Terms & Conditions</h5>
+                    <h5>Licenses</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 pt-5">
+                <h4 class = "footer_title">Social media</h4>
+                <div class="row pt-2">
+                    <img class ="twitter" src="/img/twitter-icon-18-256.png" alt="">
+                    <img class ="instagram" src="/img/white-instagram-icon-png.png" alt="">
+                </div>
+                <div class="col pt-5">
+                <h4 class = "footer_title">What is scorenow ?</h4>
+                    <h5>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam 
+                    asperiores quae ratione? 
+                    Voluptate alias ipsam, in repudiandae molestias porro ut!
+                    </h5>
+                </div>
+            </div>
+        </div>
+</div>
