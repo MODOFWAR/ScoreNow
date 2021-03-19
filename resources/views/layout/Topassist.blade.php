8fce01872841c96f59524d2335764f30745ea308
Figo Akbar <figoakbar15@gmail.com>
2021-03-19 12:01:41 +0700

    Create Topassist.blade.php

---
+++ resources/views/layout/Topassist.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('component.Head')
+</head>
+<body>
+    @include('component.Header')
+    @yield('container_topassist')
+    @include('component.Footer')
+</body>
+</html>
