e0df50e2d2487cdf23706b4f4b6374a0fd124107
Figo Akbar <figoakbar15@gmail.com>
2021-03-19 12:01:31 +0700

    Update Header.blade.php

--- resources/views/component/Header.blade.php
+++ resources/views/component/Header.blade.php
@@ -1,7 +1,7 @@
 <div class="container1">
         <nav class="navbar navbar-expand-lg navbar-light navbartop py-3">
             <div class="container-fluid">
-              <a class="navbar-brand scorenow" href="#">ScoreNow</a>
+              <a class="navbar-brand scorenow" href="#"><img src = "/img/icon.png"></a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
