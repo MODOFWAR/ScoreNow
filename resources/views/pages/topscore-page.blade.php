e7a7c05b5ffe685f45db35ab08d94f0962be5e4d
Figo Akbar <figoakbar15@gmail.com>
2021-03-19 12:02:26 +0700

    Create topscore-page.blade.php

---
+++ resources/views/pages/topscore-page.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.Topscore')
+
+@section('topscore_container')
+
+    <div class = "container pt-5">
+        <table class="table table-borderless">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Players Name</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col">Goals</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Pierre-Emerick Aubameyang</td>
+                <td></td>
+                <td></td>
+                <td style = "text-align = center">30</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Pierre-Emerick Aubameyang</td>
+                <td></td>
+                <td></td>
+                <td style = "text-align = center">30</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Pierre-Emerick Aubameyang</td>
+                <td></td>
+                <td></td>
+                <td style = "text-align = center">30</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+@endsection
