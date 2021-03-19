b271b58d28c024749514510ac7232a978f176e0f
Figo Akbar <figoakbar15@gmail.com>
2021-03-19 12:02:22 +0700

    Create topassist-page.blade.php

---
+++ resources/views/pages/topassist-page.blade.php
@@ -0,0 +1,41 @@
+@extends("layout.topassist")
+
+@section("container_topassist")
+
+<div class = "container">
+        <table class="table table-borderless">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Players Name</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col">Assists</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Pierre-Emerick Aubameyang</td>
+                <td></td>
+                <td></td>
+                <td style = "text-align = center">30</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Pierre-Emerick Aubameyang</td>
+                <td></td>
+                <td></td>
+                <td style = "text-align = center">30</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Pierre-Emerick Aubameyang</td>
+                <td></td>
+                <td></td>
+                <td style = "text-align = center">30</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+@endsection
