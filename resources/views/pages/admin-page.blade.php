b7bcfb54a74b009038bc54db2de5e5e6c28825b9
wanda-w <63240408+wanda-w@users.noreply.github.com>
2021-03-21 00:21:17 +0700

    Update admin-page.blade.php
    nerusin template page admin

--- resources/views/pages/admin-page.blade.php
+++ resources/views/pages/admin-page.blade.php
@@ -1,5 +1,53 @@
 @extends("layout.admin")
 
 @section('admin_container')
-
+<div class = "container admin tes">
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col"> Video</th>
+                    <th scope="col">Tanggal</th>
+                    <th scope="col">Deskripsi</th>
+                    <th scope="col">Foto</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td> highlight Arsenal vs Chelsea</td>
+                    <td>21-03-2021</td>
+                    <td>Video diuopload oleh adit</td>
+                    <td><img src="" alt=""></td>
+                    <td>
+                        <a href="" class="badge badge-success">Edit</a>
+                        <a href="" class="badge badge-danger">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td> highlight Arsenal vs ManCity</td>
+                    <td>20-03-2021</td>
+                    <td>-</td>
+                    <td></td>
+                    <td>
+                        <a href="" class="badge badge-success" style="text">Edit</a>
+                        <a href="" class="badge badge-danger">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td> highlight Arsenal vs ManUnited</td>
+                    <td>19-03-2021</td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <a href="" class="badge badge-success">Edit</a>
+                        <a href="" class="badge badge-danger">Delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+</div>
 @endsection
