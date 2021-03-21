9b336fdaa8ed2c1f9c391ce9f8cc8c7d6ea3646a
hafidhadhiet <hafidhadhiet@gmail.com>
2021-03-21 22:56:58 +0700

    hashed password login system

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -5,14 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class loginController extends Controller
 {
     public function postlogin (Request $request){
-        $admin = DB::table('Admin')->where('username', $request->username)->where('password', $request->password)->first();
+        // $admin = DB::table('Admin')->where('username', $request->username)->where('password', $request->password)->first();
+        // if ($admin){
+        //     return redirect('/admin-page')->with('status','Hello');
+        // }
+        // return redirect('/login-page')->with('staus','your credentials are wrong');
+
+        $admin = DB::table('admins')->where('username', $request->username)->first();
         if ($admin){
-            return redirect('/admin-page')->with('status','Hello');
+            if (Hash::check($request->password, $admin->password)){
+                return redirect('/admin-page');
+            }
+            return redirect('/login-page');
         }
-        return redirect('/login-page')->with('staus','your credentials are wrong');
+        return redirect('/login-page');
     }
 }
--- database/migrations/2021_03_21_154110_create_admins_table.php
+++ database/migrations/2021_03_21_154110_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Admin extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class Admin extends Migration
      */
     public function up()
     {
-        Schema::create('Admin', function (Blueprint $table) {
-            $table->id();
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('username');
             $table->string('password');
         });
@@ -27,6 +27,7 @@ class Admin extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('admins');
     }
+
 }
